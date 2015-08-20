b29fa62b05780a40123600abccbb5e232d4e7a9b
Daniel Potts <Daniel.Potts@tombola.com>
2015-08-20 16:32:09 +0100

    Added arrays exercises. All completed.

---
+++ arrays-exercises.js
@@ -0,0 +1,17 @@
+/**
+ * Created by daniel.potts on 20/08/15.
+ */
+//Display the first & last value of an array & CSV format
+var theArray = [3, 4, 9, 14];
+var lastNumber = theArray.pop();
+var newArray =["man", "bear", "pig"];
+var divThree = [];
+var compiledArray = (newArray.concat(theArray));
+console.log("The last number is " + lastNumber);
+console.log("The first number is " +theArray[0]);
+console.log("The entire array is " +compiledArray.join(", "));
+for(var i =0; i < theArray.length; i++ ){
+    if (theArray[i] % 3 == 0){
+        divThree.push(theArray[i]);
+        console.log(divThree + " is divisible by 3");}}
+
