996ad4a13bd1b3bc2a1220e49c2448cf8d54606d
Daniel Potts <Daniel.Potts@tombola.com>
2015-08-20 09:52:24 +0100

    Added Comparsion+and+Logical+Operators exercise.
    Completed task 1/4.
    
    Declaring two variables and outputting the largest.

---
+++ logical-operators.js
@@ -0,0 +1,15 @@
+/**
+ * Created by daniel.potts on 20/08/15.
+ */
+
+//Declare two variables and output the largest
+
+var smallNumber = 10;
+var bigNumber = 13;
+if (bigNumber > smallNumber){
+    console.log(bigNumber);}
+else {
+    console.log(smallNumber)
+}
+
+//Declare three variables and output the product and whether product is negative or not.
