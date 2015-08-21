583b9d1e88b6be82152b467dfbf20c649d660436
Daniel Potts <Daniel.Potts@tombola.com>
2015-08-21 11:17:21 +0100

    Hello world exercise has been changed.
    Returned as an array.

--- string-exercise.js
+++ string-exercise.js
@@ -2,7 +2,10 @@
  * Created by daniel.potts on 19/08/15.
  */
 //hello world
-console.log("hello world" + " JavaScript can use either “double quotes” or ‘single quotes’ to denote text.");
+var theString = "hello world" + " JavaScript can use either “double quotes” or ‘single quotes’ to denote text.";
+var theArray = [theString];
+console.log(theArray[0]);
+
 //Extracting records from the following CSV
 var records = [1997, "Ford","E350",'"Super luxurious” truck'];
     for (var i = 0; i < records.length; i++){
@@ -14,7 +17,6 @@ console.log(word.search('[AEIOU]'));
 
 //Use a regex to determine if a given string is a number.
 var isANumber = isNaN("twenty")=== false;
-
 if (isANumber) {
     console.log("Is a number");
 }
@@ -24,9 +26,6 @@ else {
 
 //Use a regex to determine Postcode validity
 var address = "I live at 19 lesbury avenue, NE28 7DR";
-
 var postcode_regex = /[A-Z]{1,2}[0-9][0-9A-Z]?\s?[0-9][A-Z]{2}/g;
-
 var postcodes = address.match(postcode_regex);
-
 console.log(postcodes);
