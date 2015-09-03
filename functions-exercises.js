71a4a923e91538b78bc2c465c27fe1de3ae5b943
Daniel Potts <Daniel.Potts@tombola.com>
2015-09-03 10:21:56 +0100

    Changes made: -Functions now return data rather than output data.

--- functions-exercises.js
+++ functions-exercises.js
@@ -2,10 +2,11 @@
  * Created by daniel.potts on 20/08/15.
  */
 //Fuel calculator: for a given MPG and distance, calculate the number of gallons of fuel needed for a trip.
-var gallons  = function (mpg, distance){
+var gallons  = function (mpg, distance) {
     var gallonsRequired = (distance / mpg);
-    console.log(gallonsRequired +" Gallons required");};
+    return gallonsRequired;};
 gallons(68,5);
+
 //Write a love calculator:
 var length = function (name1, name2) {
     var nameLength1 = name1.length,
@@ -14,13 +15,13 @@ var length = function (name1, name2) {
     result *=42;
     if (result >100) {
         result = 100;
-        console.log ("Your match is " + result + "%");}
+        return result;}
     else {
-        console.log("Your match is " + Math.round(result) + "%");}};
+        return Math.round(result);}};
 length("dan", "zoe");
 //Calculate the area of a circle for a given radius and diameter.
 var circle = function (radius){
     var area = Math.PI * (radius*radius);
-    console.log(area + "cm");};
+    return area;};
 circle(5);
 
