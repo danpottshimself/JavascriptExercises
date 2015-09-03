685ebcda102a8f3968803465bc95fc9f8b667be2
Daniel Potts <Daniel.Potts@tombola.com>
2015-09-03 10:14:52 +0100

    Changed naming of variables to full words with correct spelling.

--- exersizes-numbers.js
+++ exersizes-numbers.js
@@ -5,7 +5,7 @@
  var theNumber = 11;
 
  if (theNumber % 2 ==0){
- alert("The number " + theNumber + " is even");
+ console.log("The number " + theNumber + " is even");
  }
  else {
  console.log("The number " + theNumber + " is odd");
@@ -14,17 +14,17 @@
 
 //converting celsius to farenheit
  function convert (){
- var celc = 12;
- var faren = ((celc * 2) +30);
- console.log(celc +" degrees celsius equates to: "+faren + " "+ "farenheit");
+ var celsius = 12;
+ var fahrenheit = ((celsius * 2) +30);
+ console.log(celsius +" degrees celsius equates to: "+fahrenheit + " "+ "fahrenheit");
  }
  convert();
 
 //converting celsius to radians
 function convertToRadians () {
-    var celc = 12;
-    var radians = ((Math.PI/180) *celc);
-    console.log(celc +" degrees celsius equates to: "+ radians + " radians");
+    var celsius = 12;
+    var radians = ((Math.PI/180) *celsius);
+    console.log(celsius +" degrees celsius equates to: "+ radians + " radians");
 }
 convertToRadians();
 
@@ -42,9 +42,9 @@ console.log("The Hypotenuse length is: " + hypotenuse + " radians");
 //rolling a six sided dice
 var Die = function(sides) {
     this.sides = sides || 6;
-}
+};
 Die.prototype.roll = function() {
     return Math.floor((Math.random()*this.sides)+1);
-}
+};
 var die = new Die();
 console.log(die.roll());
