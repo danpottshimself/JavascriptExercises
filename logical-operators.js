40dcadc1d152d0cda9e5bff71f785b7a548693ee
Daniel Potts <Daniel.Potts@tombola.com>
2015-09-03 09:39:00 +0100

    Made the requested changes. Fixed: -Structure of the code in certain areas. -Removed unnecessary brackets. -Removed features that were not required.

--- logical-operators.js
+++ logical-operators.js
@@ -5,16 +5,15 @@
 
 var smallNumber = 10;
 var bigNumber = 13;
-if (bigNumber > smallNumber){
+    if (bigNumber > smallNumber){
     console.log(bigNumber);}
-else {
-    console.log(smallNumber)
-}
+    else {
+    console.log(smallNumber)}
 
 //Declare three variables and output the product and whether product is negative or not.
 var negcheck = function(value1,value2,value3) {
-    var answer = (value1 * value2) * value3;
-    console.log("The product is" + answer);
+var answer = value1 * value2 * value3;
+console.log("The product is" + answer);
     if (answer > 0) {
         console.log("Product is positive!");}
     else {
@@ -23,22 +22,18 @@ negcheck(8,-3,2);
 
 //A grading system: if the score provided is less that 40 = F, less than 50= E etc up to A. No A*, they’re just a whack idea to cover up the fact exams are too easy.
 var results = function (grades){
-    var check = grades;
-    console.log("your grade is " + check);
+var check = grades;
+console.log("your grade is " + check);
     if (check < 40) {
-        console.log("You got an F");
-    }
+        console.log("You got an F");}
         else if(check < 50) {
-            console.log("You got an E");
-        }
+            console.log("You got an E");}
          else if (check < 55){
-          console.log("You got a D");
-             }
+          console.log("You got a D");}
             else if(check <60) {
-                console.log("You got a C");
-            }
+                console.log("You got a C");}
                else if (check <70){
                     console.log("You got a B")}
-    else {
-        console.log("You are a smarty pants.");}}
+                 else if(check <80){
+                    console.log("You got an A");}}
 results(70);
