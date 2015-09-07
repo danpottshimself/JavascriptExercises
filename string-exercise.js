d69d8b35f80e43c6db044f1a1f7629d4c0787947
Daniel Potts <Daniel.Potts@tombola.com>
2015-09-07 16:49:18 +0100

    Changed the cars into a string and then displayed it as an array in CSV format.
    Changed the ragex term to use a + so that it looks out for other characters such as decimals.

--- string-exercise.js
+++ string-exercise.js
@@ -7,10 +7,10 @@ var theArray = theString.split(" ");
 console.log(theArray);
 
 //Extracting records from the following CSV
-var records = [1997, "Ford","E350",'"Super luxurious” truck'];
-    for (var i = 0; i < records.length; i++){
-    console.log("This record is " + records[i]);
-}
+var records = "1997 Ford E350 'Super luxurious” truck'";
+var endArray = records.split(" ");
+    console.log("This record is " + endArray);
+
 //Find the position of the first vowel in the text
 var word ="JSDFHDVHUEVDAKNDSEINX";
 console.log(word.search('[AEIOU]'));
@@ -18,7 +18,7 @@ console.log(word.search('[AEIOU]'));
 //Use a regex to determine if a given string is a number.
 var numberCheck = function (sentence) {
    sentence = "This contains 1 number";
-    var number = /[0-9]/g;
+    var number = /[0-9]+/g;
     if (sentence.match(number)) {
         console.log("Contains a number")
     }
